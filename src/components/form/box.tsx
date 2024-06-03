dca118a25ede0b61f95ad645eeedfb3c3981b7ea
o6ez9na <f1stingday@gmail.com>
2024-06-04 02:20:48 +0300

    init proj

---
+++ src/components/form/box.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export default function Box() {
+  return <div className="wrap">
+    <div className="this-day-block">
+
+    </div>
+    <div className="idk">
+
+    </div>
+    <div className="weak-block"></div>
+  </div>;
+}
