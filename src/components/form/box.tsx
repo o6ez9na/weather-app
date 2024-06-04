2471efa9f28d30ed13d7f6e9d1e91babc9328c31
o6ez9na <f1stingday@gmail.com>
2024-06-04 22:26:25 +0300

    todo

--- src/components/form/box.tsx
+++ src/components/form/box.tsx
@@ -1,15 +1,14 @@
 import "./style.css";
-
+/**
+ * ! information block is not realisated
+ * ! need API
+ */
 export default function Box() {
-  return <div className="wrap">
-    <div className="this-day-block">
-
+  return (
+    <div className="wrap">
+      <div className="this-day-block"></div>
+      <div className="inforamtion-block"></div>
+      <div className="weak-block"></div>
     </div>
-    <div className="idk">
-
-    </div>
-    <div className="weak-block">
-      
-    </div>
-  </div>;
+  );
 }
